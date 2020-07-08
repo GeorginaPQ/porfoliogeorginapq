6404143fac729d90f28e07ef398b72c67c5f95a0
GeorginaPQ <giorginapqe@gmail.com>
2020-07-08 06:53:42 -0500

    Updates

---
+++ precache-manifest.739c199f441259a65297639b60436d02.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dee5c33479b1a80c63fe85149ed6851",
+    "url": "/portfolioGeorginaPQ/index.html"
+  },
+  {
+    "revision": "dddc56d3f2380f6306ed",
+    "url": "/portfolioGeorginaPQ/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "dddc56d3f2380f6306ed",
+    "url": "/portfolioGeorginaPQ/static/js/2.eddab126.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolioGeorginaPQ/static/js/2.eddab126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d376f4e8e95e120de14",
+    "url": "/portfolioGeorginaPQ/static/js/main.2ed63a87.chunk.js"
+  },
+  {
+    "revision": "e6273cab82db1d559807",
+    "url": "/portfolioGeorginaPQ/static/js/runtime-main.05688464.js"
+  },
+  {
+    "revision": "c93f86cd659cb72347c6eeb5fb6d611d",
+    "url": "/portfolioGeorginaPQ/static/media/SocialNetwork.c93f86cd.png"
+  },
+  {
+    "revision": "cde600735213a95af56db4b8f1f76485",
+    "url": "/portfolioGeorginaPQ/static/media/UrbanCat.cde60073.png"
+  },
+  {
+    "revision": "24a057c7c871744633a3092887e0431f",
+    "url": "/portfolioGeorginaPQ/static/media/celebration.24a057c7.png"
+  },
+  {
+    "revision": "d40a785e10f954d0b49f4b3ee3c58409",
+    "url": "/portfolioGeorginaPQ/static/media/geo.d40a785e.png"
+  },
+  {
+    "revision": "d5ae6be0426da26af76839f088589826",
+    "url": "/portfolioGeorginaPQ/static/media/logowhite.d5ae6be0.svg"
+  },
+  {
+    "revision": "cdd8ed0dacf8ad7f206381df14d8abd2",
+    "url": "/portfolioGeorginaPQ/static/media/movies.cdd8ed0d.png"
+  }
+]);
